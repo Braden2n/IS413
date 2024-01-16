1af16af7cf1edb567e5b58617edac06f873f2675
Braden2n <103770034+Braden2n@users.noreply.github.com>
2024-01-15 19:50:22 -0700

    Added Mission2 Folder

---
+++ Mission2/DiceRoller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Mission2
+{
+    /// <summary>
+    /// Class <c>DiceRoller</c> manages the simulation of rolling 2 dice n times.
+    /// </summary>
+    internal class DiceRoller
+    {
+        // Single instande of Random generator
+        private readonly Random rnd = new Random();
+        public DiceRoller() { }
+        /// <summary>
+        /// Method <c>Simulate</c> simulates the rolling of two dice num_rolls times.
+        /// Method <c>Roll</c> is called to simulate a two-dice roll.
+        /// Array of 11 occurences  is returned (pos[0] == 2; pos[10] == 12)
+        /// </summary>
+        public int[] Simulate(int num_rolls)
+        {
+            // Empty array for incrementing
+            int[] roll_results = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < num_rolls; i++)
+            {
+                // Roll will return a minimum of 2, which gets adjusted to pos[0]
+                roll_results[Roll() - 2] += 1;
+            }
+            return roll_results;
+        }
+        /// <summary>
+        /// Simulates the rolling of two dice.
+        /// </summary>
+        private int Roll()
+        {
+            // Two random rolls are made
+            return rnd.Next(1, 7) + rnd.Next(1,7);
+        }
+    }
+}
---
+++ Mission2/Simulate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Mission2;
+
+namespace Mission2
+{
+    internal class Simulate
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
+            int rolls = Convert.ToInt32(Console.ReadLine());
+            // Calculating how many values a single percent is
+            float one_percent = (float)rolls / 100;
+            if (one_percent < 0)
+            {
+                one_percent = 1 / one_percent;
+            }
+            DiceRoller roller = new DiceRoller();
+            int[] results = roller.Simulate(rolls);
+            Console.WriteLine("\nSIMULATION RESULTS\nEach \"*\" represents 1% of the total rolls\nTotal rolls = " + rolls + "\n");
+            for (int i = 0; i < results.Length; i++)
+            {
+                // Calculating what percentage of rolls the value had
+                int percentage = Convert.ToInt32(Math.Floor(results[i] / one_percent));
+                Console.WriteLine((i + 2) + ":\t" + new string('*', percentage));
+            }
+        }
+    }
+}
