16467fe328c4d73d4041483b62fa87a0059be36c
Braden2n <103770034+Braden2n@users.noreply.github.com>
2024-01-17 13:51:55 -0700

    Updated code - small improvements

--- Mission2/DiceRoller.cs
+++ Mission2/DiceRoller.cs
@@ -2,37 +2,23 @@
 
 namespace Mission2
 {
-    /// <summary>
-    /// Class <c>DiceRoller</c> manages the simulation of rolling 2 dice n times.
-    /// </summary>
     internal class DiceRoller
     {
-        // Single instande of Random generator
+        // RNG Generator
         private readonly Random rnd = new Random();
+        // Constructor
         public DiceRoller() { }
-        /// <summary>
-        /// Method <c>Simulate</c> simulates the rolling of two dice num_rolls times.
-        /// Method <c>Roll</c> is called to simulate a two-dice roll.
-        /// Array of 11 occurences  is returned (pos[0] == 2; pos[10] == 12)
-        /// </summary>
+        // Simulator of `num_rolls` two-dice rolls
         public int[] Simulate(int num_rolls)
         {
-            // Empty array for incrementing
-            int[] roll_results = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            // Result array with pos. 0-10 (2-12 - 2)
+            int[] roll_results = new int[11];
             for (int i = 0; i < num_rolls; i++)
             {
-                // Roll will return a minimum of 2, which gets adjusted to pos[0]
-                roll_results[Roll() - 2] += 1;
+                // Addition of the RNG will be 2-12 - adjusting for 0-10 pos.
+                roll_results[rnd.Next(1, 7) + rnd.Next(1, 7) - 2]++;
             }
             return roll_results;
-        }
-        /// <summary>
-        /// Simulates the rolling of two dice.
-        /// </summary>
-        private int Roll()
-        {
-            // Two random rolls are made
-            return rnd.Next(1, 7) + rnd.Next(1,7);
         }
     }
 }
--- Mission2/Simulate.cs
+++ Mission2/Simulate.cs
@@ -9,20 +9,19 @@ namespace Mission2
         {
             Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
             int rolls = Convert.ToInt32(Console.ReadLine());
-            // Calculating how many values a single percent is
+            // Calculating what 1% of rolls is
             float one_percent = (float)rolls / 100;
-            if (one_percent < 0)
-            {
-                one_percent = 1 / one_percent;
-            }
+            // Instantiate simulator
             DiceRoller roller = new DiceRoller();
+            // Getting array of simulated results
             int[] results = roller.Simulate(rolls);
             Console.WriteLine("\nSIMULATION RESULTS\nEach \"*\" represents 1% of the total rolls\nTotal rolls = " + rolls + "\n");
             for (int i = 0; i < results.Length; i++)
             {
-                // Calculating what percentage of rolls the value had
-                int percentage = Convert.ToInt32(Math.Floor(results[i] / one_percent));
-                Console.WriteLine((i + 2) + ":\t" + new string('*', percentage));
+                // Displaying one asterisks per percentage each value had
+                Console.WriteLine((i + 2) + ":\t" + new string(
+                    '*', Convert.ToInt32(Math.Floor(results[i] / one_percent
+                ))));
             }
         }
     }
